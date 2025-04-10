eadd66aee67193396a8e37bd110736cb7cba4cf6
Tanishka Chaudhary <chaudharytanishka38@gmail.com>
2025-04-10 09:30:55 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Render from "./RenderPara";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Render></Render>
+    </div>
   )
 }
 
---
+++ src/components/RenderPara.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+const Render = () =>{
+   return (
+    
+     <div id="root">
+
+      <p>I am learning React. My life is getting better.</p>
+
+     </div>
+
+   )
+}
+
+
+export default Render;
